c6e61cb1871e10e5b194fedf1e65bb1ef54d9a84
PinkerToad <kevinbiczkowski@gmail.com>
2023-05-30 19:07:08 +0200

    Update comparisons.py

---
+++ comparisons.py
@@ -0,0 +1,6 @@
+#       >=      means that it needs to be higher or equal
+#       ==      means that it needs to be equal
+#       <=      means that it needs to be lower or equal
+#       >       means that it needs to be higher
+#       <       means that it needs to be lower 
+#       !=      means that it needs to be unequal
